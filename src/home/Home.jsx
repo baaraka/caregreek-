333f97ad99cfbc41e79d3065492921dc6100fadd
barakaganai <barakaganai01@gmail.com>
2022-09-06 14:16:13 +0300

    create home.jsx component

---
+++ src/home/Home.jsx
@@ -0,0 +1,30 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="homeImg">
+        <img
+          src="https://images.pexels.com/photos/45842/clasped-hands-comfort-hands-people-45842.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt=""
+          className="homeImgItem"
+        />
+      </div>
+      <div className="homeTitle">
+        <h1 className="homeTitleItem">About Us</h1>
+      </div>
+      <div className="homeList">
+        <ul className="homeListItem">
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
