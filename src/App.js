85386c7af44267f91d61df78832e14675fd961ca
barakaganai <barakaganai01@gmail.com>
2022-09-06 14:17:22 +0300

    import Home component in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import About from "./about/About";
 import "./App.css";
+import Home from "./home/Home";
 import TopBar from "./topBar/TopBar";
 
 export default function App() {
   return (
     <div className="app">
       <TopBar />
+      <Home />
       <About />
     </div>
   );
